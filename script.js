a0d2d5628cfcfc6f715140d653a35135e11f2d6b
Derrick Owusu-Asare <aderrick649@gmail.com>
2025-10-06 15:15:47 +0000

    done

--- script.js
+++ script.js
@@ -3,6 +3,36 @@ document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("registration-form")
     const feedbackDiv = document.getElementById("form-feedback")
 
-
+    document.addEventListener("submit", (event) => {
+        event.preventDefault();
+    })
+    const username = document.getElementById("username").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+    let isValid = true;
+    let messages = [];
+    if(username.length < 3){
+        isValid = false;
+        messages.push("Username must be more than 3 characters")
+    }
+    if(!email.includes("@") || !email.includes(".")){
+        isValid =  false;
+        messages.push("Please enter a valid email")
+    }
+    if(password.length < 8){
+        isValid = false;
+        messages.push("Password should be longer than 8 character")
+    }
+    feedbackDiv.style.display = "block";
+    if (isValid){
+        feedbackDiv.textContent = "Registration successful!"
+        feedbackDiv.style.color = #28a745
+    }
+    if (!isValid) {
+        feedbackDiv.innerHTML = messages.join("<br>");
+        feedbackDiv.style.color = "#dc3545"; // red color for errors
+}
 
 });
+
+
