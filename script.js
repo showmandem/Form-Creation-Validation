64f919d87fed73d7a908a6b8314d68b0b3022bf6
Derrick Owusu-Asare <aderrick649@gmail.com>
2025-10-06 14:13:41 +0000

    pushing

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // All my JavaScript code goes inside this function
+  console.log("The HTML document has been fully loaded!");
+
+
+  
+});
