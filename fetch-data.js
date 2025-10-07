f474678e538ae0b69e1b1ad9cfe1af57833e91ac
Derrick Owusu-Asare <aderrick649@gmail.com>
2025-10-07 19:09:43 +0000

    changes to file names

--- fetch-data.js
+++ fetch-data.js
@@ -1,11 +1,13 @@
 async function fetchUserData() {
     const apiUrl = 'https://jsonplaceholder.typicode.com/users';
     const dataContainer = document.getElementById('api-data');
-}
+
 try{
     const response = await fetch(apiUrl);
-    const users = await response.json()
+    const users = await response.json();
+    console.log(users);
 }
 catch(error){
-    // console.error("Error with data :" error);
-}
+    console.error('Error fetching data:', error);
+    dataContainer.textContent = 'Failed to load user data.';
+}}
